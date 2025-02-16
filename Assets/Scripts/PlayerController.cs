a1cfa1e6bfdd3b3d7324e4a9423e8b07865ff35b
Evan Gentile <ewgentile@gmail.com>
2025-02-15 20:36:10 -0500

    dash?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -14,9 +15,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundCheckX = 0.5f;
     [SerializeField] private LayerMask whatIsGround;
 
+    //Dashing
+    [SerializeField] private float dashingVelocity = 14f;
+    [SerializeField] private float dashingTime = 0.5f;
+    private Vector2 dashingDir;
+    private bool isDashing;
+    private bool canDash = true;
+
+    //Components
+    private TrailRenderer trailRenderer;
+    private Rigidbody2D rigidBody2D;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
+
+
     }
 
     void Update()
@@ -24,7 +39,51 @@ public class PlayerController : MonoBehaviour
         GetInputs();
         Move();
         Jump();
+
+        var dashInput = Input.GetButtonDown("Dash");
+
+        if (dashInput && canDash)
+        {
+            isDashing = true;
+            canDash = false;
+            trailRenderer.emitting = true;
+            dashingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            if (dashingDir == Vector2.zero)
+            {
+                dashingDir = new Vector2(transform.localScale.x, 0);
+
+            }
+
+            StartCoroutine(StopDashing());
+
+
+        }
+
+
+        if (isDashing)
+        {
+            rigidBody2D.linearVelocity = dashingDir.normalized * dashingVelocity;
+            return;
+        }
+
+        if (Grounded())
+        {
+            canDash = true;
+        }
+
+
     }
+
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
+
+
+    }
+
 
     void GetInputs()
     {
